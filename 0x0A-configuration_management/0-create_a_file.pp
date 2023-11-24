b832ec90e241953d82fc9b196e5035548d2dcddc
Selomon1 <soya.vandy1950@gmail.com>
2023-11-24 10:30:02 -0800

    Con Man

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,9 @@
 # Create a file named 'school' in /tmp using puppet
 
 file {'/tmp/school':
-  ensure => file,
-  mode   => '0744',
-  owner  => 'www-data',
-  group  => 'I love Puppet',
+  ensure  => file,
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet',
 }

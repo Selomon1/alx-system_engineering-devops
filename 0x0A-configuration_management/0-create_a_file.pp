8795c913d159164c36d44e49f7a35d01617e419d
Selomon1 <soya.vandy1950@gmail.com>
2023-11-24 10:15:50 -0800

    Config Management

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,10 @@
+#!/usr/bin/pup
+
+# Create a file named 'school' in /tmp using puppet
+
+file {'/tmp/school':
+  ensure => file,
+  mode   => '0744',
+  owner  => 'www-data',
+  group  => 'I love Puppet',
+}

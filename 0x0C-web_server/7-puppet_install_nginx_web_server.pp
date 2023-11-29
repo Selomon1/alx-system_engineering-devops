ccaca5ffb7f893e7d4ab767816982d8ac5119fe2
Selomon1 <soya.vandy1950@gmail.com>
2023-11-29 10:44:27 -0800

    web server

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,19 @@
+# Install and configure nginx
+package { 'nginx':
+  ensure => 'installed',
+}
+
+file_line { 'redirect-me':
+  ensure => 'present',
+  path   => '/etc/nginx/sites-available/default',
+  after  => 'listen 80 default_server',
+  line   => rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
+}
+
+file { '/var/www/html/index.html':
+  content => 'Hello World!',
+}
+
+service { 'nginx':
+  ensure   => running,
+  required => Package['nginx']

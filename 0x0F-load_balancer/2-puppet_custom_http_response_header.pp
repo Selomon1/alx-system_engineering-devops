24b404c50b5d10ed808baef7d87f1765b9097676
Selomon1 <soya.vandy1950@gmail.com>
2023-12-06 18:14:34 +0300

    Load Balancer

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -11,11 +11,12 @@ exec { 'update':
 -> file_line { 'add_header':
   ensure => present,
   path   => '/etc/nginx/sites-available/default',
-  line   => "\tadd_header X-Served-By '$HOSTNAME';",
+  line   => "\tadd_header X-Served-By '${facts['hostname']};",
   after  => "server_name _;"
 }
 
 -> exec { 'restart service':
   command  => 'sudo service nginx restart',
   provider => shell,
+  require  => Package['nginx'],
 }

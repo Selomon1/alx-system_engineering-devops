eba3040d09595448e395f1f63ebcf5466f91671b
Selomon1 <soya.vandy1950@gmail.com>
2023-12-06 18:21:55 +0300

    Load Balancer

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,21 +1,22 @@
+#!/usr/bin/env bash
 # HTTP header response usind pappet
 exec { 'update':
   command  => 'sudo apt-get update',
   provider => shell,
 }
 
-package { 'nginx':
-  ensure => present,
+-> package { 'nginx':
+  ensure => installed,
 }
 
-file_line { 'add_header':
+-> file_line { 'add_header':
   ensure => present,
   path   => '/etc/nginx/sites-available/default',
   line   => "\tadd_header X-Served-By ${hostname};",
   after  => "server_name _;"
 }
 
-exec { 'restart service':
+-> exec { 'restart_service':
   command  => 'sudo service nginx restart',
   provider => shell,
   require  => Package['nginx'],

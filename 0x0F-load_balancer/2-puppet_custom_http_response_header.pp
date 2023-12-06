259b35352fb9cf1093e1aad17121f142c30378de
Selomon1 <soya.vandy1950@gmail.com>
2023-12-06 18:02:48 +0300

    Load Balancer

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,36 +1,21 @@
 # Install Nginx package using pappet
-package { 'nginx':
+exec { 'update':
+  command  => 'sudo apt-get update',
+  provider => shell,
+}
+
+-> package { 'nginx':
   ensure => installed,
 }
 
-# configure HTTP header
-file { '/etc/nginx/html/index.html':
-  content => 'Hello World!',
-}
-
 # configure HTTP server
-file_line { 'add_header':
+-> file_line { 'add_header':
   ensure => present,
   path   => '/etc/nginx/sites-available/default',
   line   => "\tadd_header X-Served-By '$HOSTNAME';",
   after  => "server_name _;"
-
-# redirect
-file_line { 'redirect':
-  ensure => present,
-  path   => '/etc/nginx/sites-available/default',
-  after  => "add_header X-Server-By '$HOSTNAME'",
-  line   => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
 }
 
-# 404 error page
-file { '/etc/nginx/html/404_page_error.html':
-  ensure => file,
-  content => "Ceci n'est pas une page",
-  require => Package['nginx'],
-}
-
-# Define service
 service { 'nginx':
   ensure  => running,
 }

9dca84a2d78c87e65d31e3667e4c053a9fc6d61e
Selomon1 <soya.vandy1950@gmail.com>
2024-01-02 18:44:57 +0300

    API

---
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+Script that retrieves and export data in the Json format
+"""
+import requests
+import json
+
+if __name__ == '__main__':
+    b_url = "https://jsonplaceholder.typicode.com"
+    users = requests.get(f"{b_url}/users").json()
+    todos = requests.get(f"{b_url}/todos").json()
+
+    todo_all_employees = {}
+
+    for user in users:
+        user_id = user.get("id")
+        username = user.get("username")
+        user_todos = [
+            {
+                "username": username,
+                "task": todo.get("title"),
+                "completed": todo.get("completed")
+            }
+            for todo in todos if todo.get("userId") == user_id
+        ]
+        todo_all_employees[str(user_id)] = user_todos
+
+    with open("todo_all_employees.json", "w") as json_file:
+        json.dump(todo_all_employees, json_file)

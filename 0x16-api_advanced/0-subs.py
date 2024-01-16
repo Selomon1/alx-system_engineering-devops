03a88c3e2d83fd77fd0298f8b35bc256d1552529
Selomon1 <soya.vandy1950@gmail.com>
2024-01-16 21:17:59 +0300

    Api Advanced

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,7 +7,7 @@ def number_of_subscribers(subreddit):
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {'User-Agent': 'Api_project'}
 
-    res = requests.get(url, headers=headers, allow_redirects=False)
+    res = requests.get(url, headers=headers)
 
     if subreddit is None or not isinstance(subreddit, str):
         return 0

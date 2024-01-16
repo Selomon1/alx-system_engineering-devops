18b2c7778653d6b1e133af6008d52e86cf736776
Selomon1 <soya.vandy1950@gmail.com>
2024-01-16 20:16:16 +0300

    Api Advanced

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -12,9 +12,9 @@ def number_of_subscribers(subreddit):
     res = requests.get(url, headers=headers, allow_redirects=False)
 
     if res.status_code >= 300:
-        return (0)
+        return 0
     else:
         data = res.json()
         data_dic = data.get('data')
         subscribers = data_dic.get('subscribers')
-        return (subscribers)
+        return subscribers

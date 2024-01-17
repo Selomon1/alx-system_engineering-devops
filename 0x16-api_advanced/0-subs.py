22d24aa1af0e1b686564b20cc1b3ad381f36a25a
root <root@LAPTOP-203ITEFHNM>
2024-01-17 11:43:19 +0300

    Api Advanced

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -6,11 +6,13 @@ import requests
 def number_of_subscribers(subreddit):
     """ Returns the number of subscribers """
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {'User-Agent': 'Api_project/requests:v1.0.0 (by /u/Selomon1)'}
+    headers = {'User-Agent': 'request'}
     res = requests.get(url, headers=headers, allow_redirects=False)
+
     if res.status_code != 200:
         return 0
 
     r_data = res.json()
     subscribe = r_data.get('data', {}).get('subscribers', 0)
+    
     return subscribe

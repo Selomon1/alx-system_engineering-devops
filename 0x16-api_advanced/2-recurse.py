08fb25a76d6ff5e735c9af3d9b894d16636faab0
Selomon1 <soya.vandy1950@gmail.com>
2024-01-17 17:29:10 +0300

    Api Advanced

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -19,7 +19,7 @@ def recurse(subreddit, hot_list=[], after=None, count=0):
             allow_redirects=False
         )
 
-        if r.status_code != 200:
+        if r.status_code == 404:
             return None
 
         data = r.json().get('data', {})

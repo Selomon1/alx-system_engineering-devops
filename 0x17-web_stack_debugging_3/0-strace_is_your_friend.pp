b007f37682ffe9d0ba34c6b798d80d2cdccc28a6
Selomon1 <soya.vandy1950@gmail.com>
2024-01-17 14:55:34 +0300

    Web Stack Debugging 3

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,11 +1,6 @@
 # Fix the problem identified
-file { '/var/www/html/wp-settings.php':
-  ensure  => file,
-  content => inline_template('<%= File.read("/var/www/html/wp-settings.php").gsub("phpp", "php") %>'),
-  notify  => Exec['reload-apache'],
-}
 
-exec { 'reload-apache':
-  command => 'service apache2 reload',
-  refreshonly => true,
+exec { 'fix-wordpress':
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  path    => 'usr/local/bin/:/bin/',
 }

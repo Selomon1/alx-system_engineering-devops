c92deaf5d3d0c98a0fc86d77150caef6a554d664
Selomon1 <soya.vandy1950@gmail.com>
2024-01-17 14:45:24 +0300

    Web Stack Debugging 3

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,29 +1,11 @@
-# Ensure Apache is installed
-package { 'apache2':
-  ensure => 'installed',
-}
-
-# Ensure Apache is running
-service { 'apache2':
-  ensure => 'running',
-}
-
-# Execute strace to find the problem and fix
-exec { 'fix-apache-error':
-  command => '/usr/bin/strace -f -e trace=open,write -s 1024 -o /tmp/strace_output.txt service apache2 restart',
-  path    => ['/bin', '/usr/bin', '/usr/sbin'],
-  creates => '/tmp/strace_output.txt',
-  require => [Package['apache2'], Service['apache2']],
-}
-
 # Fix the problem identified
-file { '/etc/apache2/sites-available/000-default.conf':
+file { '/var/www/html/wp-settings.php':
   ensure  => file,
-  content => file('/etc/apache2/sites-available/000-default.conf').content.gsub('old_string', 'new_string'),
-  notify  => Service['apache2'],
+  content => inline_template('<%= File.read("/var/www/html/wp-settings.php").gsub("phpp", "php") %>'),
+  notify  => Exec['reload-apache'],
 }
-# Notify the user when the fix is done
-notify { 'Apache Fix Applied':
-  message => 'The Apache 500 error has been fixed!',
-  require => Exec['fix-apache-error'],
+
+exec { 'reload-apache':
+  command => 'service apache2 reload',
+  refreshonly => true,
 }
